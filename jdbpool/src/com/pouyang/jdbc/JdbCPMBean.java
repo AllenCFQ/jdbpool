f72861ee8ae4b03c142a520533ae283a9d0544f2
ouyangping <ouyangpingfly@163.com>
2015-02-26 14:28:56 +0800

    创建接口

---
+++ jdbpool/src/com/pouyang/jdbc/JdbCPMBean.java
@@ -0,0 +1,15 @@
+/*
+ * @(#)JdbCPMBean.java Feb 26, 2015
+ * Copyright 2015 OUYANGPING All rights reserved.
+ * contact information:ouyangpingfly@163.com
+ */
+
+package com.pouyang.jdbc;
+
+/**
+ * @author ouyangping
+ * @date Feb 26, 2015 2:23:33 PM
+ */
+public abstract interface JdbCPMBean {
+  public abstract String getPoolName();
+}
